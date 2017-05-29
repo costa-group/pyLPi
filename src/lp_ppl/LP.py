6c3d08f0e16644edd2d4b40a5b42fc7cf5bdc933
Jesus Domenech <jesusjda@gmail.com>
2017-05-29 18:33:19 +0200

    init

---
+++ src/lp_ppl/LP.py
@@ -0,0 +1,8 @@
+import ppl
+
+class Variable(ppl.Variable):
+    def imhere(self):
+        pass
+class Linear_Expression(ppl.Linear_Expression):
+    def imhere(self):
+        pass
---
+++ src/lp_z3/LP.py
@@ -0,0 +1,39 @@
+import ppl
+import z3
+
+class Variable(ppl.Variable):
+    def imhere(self):
+        pass
+class Linear_Expression(ppl.Linear_Expression):
+    def imhere(self):
+        pass
+class Constraint(ppl.Constraint):
+    def imhere(self):
+        pass
+class Constraint_System:
+    def __init__(self,c=[]):
+        if(isinstance(c,ppl.Constraint)):
+            self.cons = [c]
+        elif(isinstance(c,list)):
+            self.cons = c
+        else:
+            raise ValueError("Constraint or list of Constraints")
+    def insert(self, c):
+        self.cons.append(c)
+
+    def has_strict_inequalities(self):
+        for c in self.cons:
+            if(c.is_strict_inequality()):
+                return True
+        return False
+    def has_equalities(self):
+        for c in self.cons:
+            if(c.is_equality()):
+                return True
+        return False
+
+    def empty(self):
+        return len(self.cons)==0
+
+    def clear(self):
+        self.cons = []
