3b49954cb80aa67fd5eda7f50d9011755c7b380d
Jesus Domenech <jesusjda@gmail.com>
2017-05-31 19:53:24 +0200

    LPppl interface ready!

---
+++ src/LPppl.py
@@ -0,0 +1,6 @@
+from ppl import Variable
+from ppl import Linear_Expression
+from ppl import Constraint
+from ppl import Constrain_System
+from ppl import Polyhedron
+from ppl import C_Polyhedron
--- src/lp_ppl/LP.py
+++
@@ -1,1 +0,0 @@
-from ppl import Variable
