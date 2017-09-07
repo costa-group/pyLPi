127e3aa7c94d858ec88ec4b0ea9c38f4a3c3fe7f
Jesus Domenech <jesusjda@gmail.com>
2017-09-07 20:32:44 +0200

    basic test

---
+++ test/test_LPi.py
@@ -0,0 +1,19 @@
+import unittest
+import lpi
+
+
+class TestKey(unittest.TestCase):
+    def test_import(self):
+        lpi.C_Polyhedron()
+        self.assertEqual(1, 1)
+
+    def test_ppl(self):
+        lpi.C_Polyhedron(lplib="ppl")
+        self.assertEqual(1, 1)
+
+    def test_z3(self):
+        try:
+            lpi.C_Polyhedron(lplib="z3")
+        except NotImplementedError as e:
+            self.skipTest("z3 is not implemented")
+        self.assertEqual(1, 1)
