51e3370d746d454b8bb4f6c2cfd1df6e85ef7669
Jesus Domenech <jesusjda@gmail.com>
2017-12-01 14:44:38 +0100

    ppl installation

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ VERSION = open(os.path.join(base, 'version.txt')).read()[:-1]
 pkg_dir = os.path.join(base, 'lpi')
 pkg_name = 'lpi'
 
-requires = ['pplpy>=0.7', 'z3-solver']
+requires = ['pplpy>=0.7.1', 'z3-solver']
 
 dependency_links = [
-    'git+https://github.com/jesusjda/pplpy.git#egg=pplpy-0.7'
+    'git+https://github.com/jesusjda/pplpy.git#egg=pplpy-0.7.1'
 ]
 
 setup(
